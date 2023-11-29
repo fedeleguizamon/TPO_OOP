8f0182c3e0b681eed5d6a7862b9421802bc01d28
fedeleguizamon <111534221+fedeleguizamon@users.noreply.github.com>
2023-11-29 19:42:24 -0300

    Delete TPO_OOP directory

--- TPO_OOP/src/main/java/com/tpo_oop/repositories/ExoPlanetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tpo_oop.repositories;
-
-import com.tpo_oop.entities.ExoPlanet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExoPlanetRepository extends JpaRepository<ExoPlanet, Integer> {
-}
--- TPO_OOP/src/main/java/com/tpo_oop/repositories/SatelliteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tpo_oop.repositories;
-
-import com.tpo_oop.entities.Satellite;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SatelliteRepository extends JpaRepository<Satellite, Integer> {
-}
