ded4577025af8b3e071f2dce3f755722ed6f11c3
fedeleguizamon <111534221+fedeleguizamon@users.noreply.github.com>
2023-12-02 23:01:11 -0300

    Delete TPO_OOP directory

--- TPO_OOP/src/main/java/com/tpo_oop/exceptions/NoSpacialObserversException.java
+++
@@ -1,7 +0,0 @@
-package com.tpo_oop.exceptions;
-
-public class NoSpacialObserversException extends Exception {
-    public NoSpacialObserversException(String message) {
-        super(message);
-    }
-}
--- TPO_OOP/src/main/java/com/tpo_oop/repositories/SpacialObserverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tpo_oop.repositories;
-
-import com.tpo_oop.entities.SpacialObserver;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpacialObserverRepository extends JpaRepository<SpacialObserver, Integer> {
-}
